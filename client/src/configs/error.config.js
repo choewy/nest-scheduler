d4156544a8c7d83e42c0c59f3c37ceafb7726f8a
choewy <choewy32@gmail.com>
2022-03-27 20:13:40 +0900

    로그인 오류 정의

--- client/src/configs/error.config.js
+++ client/src/configs/error.config.js
@@ -4,3 +4,8 @@ export const SignUpErrors = {
     password: '비밀번호는 영문+숫자+특수문자 8자 이상으로 입력하세요.',
     confirmPassword: '비밀번호가 일치하지 않습니다.'
 };
+
+export const SignInErrors = {
+    email: '이메일 형식에 맞지 않습니다.',
+    password: '비밀번호를 확인하세요.'
+};
