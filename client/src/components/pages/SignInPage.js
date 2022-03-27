7b62b578e6a3acee30bb4e7adbbbb1f5d997962a
choewy <choewy32@gmail.com>
2022-03-27 15:24:20 +0900

    SignInPage

--- client/src/components/pages/SignInPage.js
+++ client/src/components/pages/SignInPage.js
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+
 const SignInPage = () => {
+    const [state, setState] = useState({
+        email: '',
+        password: '',
+    });
+
+    const stateChangedHandler = (e) => {
+        const { taget: { name, value } } = e;
+        // watcher & pipe
+
+        setState({ ...state, [name]: value });
+    };
+
+    const signInHandler = (e) => {
+        e.preventDefault();
+        console.log(state);
+    };
+
     return (
         <div>
-            로그인 페이지
+            로그입 페이지
+            <form onSubmit={signInHandler}>
+                <div>
+                    <input type="email"
+                        name="email"
+                        value={state.email}
+                        onChange={stateChangedHandler} />
+                </div>
+                <div>
+                    <input type="password"
+                        name="password"
+                        value={state.password}
+                        onChange={stateChangedHandler} />
+                </div>
+                <div>
+                    <button type="submit">로그인</button>
+                </div>
+            </form>
         </div>
     );
 };
