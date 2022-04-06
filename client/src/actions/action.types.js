6e9620b211d3aa58004fc565adb55db29387027d
최원영 <70950533+choewy@users.noreply.github.com>
2022-04-06 16:46:45 +0900

    Delete action.types.js

--- client/src/actions/action.types.js
+++
@@ -1,5 +0,0 @@
-// (미사용) Redux Reducer Action
-export const SIGNIN_USER = 'signin_user';
-export const SIGNUP_USER = 'signup_user';
-export const SIGNOUT_USER = 'signout_user';
-export const AUTH_USER = 'auth_user';
