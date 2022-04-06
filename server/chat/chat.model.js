b835984cefbf82c44fc9dfc2834ade7852b7fb43
choewy <choewy32@gmail.com>
2022-04-06 16:47:50 +0900

    schema + model 통합

--- server/chat/chat.model.js
+++ server/chat/chat.model.js
@@ -1,7 +1,18 @@
 'use strict';
 
+const { Schema } = require('mongoose');
 const { model } = require('mongoose');
-const ChatSchema = require('./chat.schema');
+
+const ChatSchema = Schema({
+    message: {
+        type: String,
+    },
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+}, { timestamps: true });
+
 const Chat = model('Chat', ChatSchema);
 
 module.exports = { Chat };
